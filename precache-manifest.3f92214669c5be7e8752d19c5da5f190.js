86b28c20c1efd71bbbd0b11d8594591e2622b0f5
Ton0703 <1120367213@qq.com>
2020-08-05 19:29:24 +0800

    Updates

---
+++ precache-manifest.3f92214669c5be7e8752d19c5da5f190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3ed4611632ae20265446fcf3885ef7",
+    "url": "/react-trello/index.html"
+  },
+  {
+    "revision": "48326c833db87328799f",
+    "url": "/react-trello/static/css/main.8fdaa3e8.chunk.css"
+  },
+  {
+    "revision": "d251059511ca89739790",
+    "url": "/react-trello/static/js/2.4230cadd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-trello/static/js/2.4230cadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48326c833db87328799f",
+    "url": "/react-trello/static/js/main.e43f3f27.chunk.js"
+  },
+  {
+    "revision": "14dbdaf945c8db93cfb1",
+    "url": "/react-trello/static/js/runtime-main.d1f7db2d.js"
+  }
+]);
